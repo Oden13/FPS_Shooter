6d38c49a60aff2e88287b0a9b1894ac1c0e37c3d
Peyton Paul <peyton.z.paul@gmail.com>
2021-03-31 06:38:45 -0400

    Added Screens

---
+++ First_Person_Shooter/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool gamePaused = false;
+    public GameObject pauseMenu;
+    void Start () {
+
+    }
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.P)) {
+            if (gamePaused) {
+                ResumeGame ();
+            } else {
+                PauseGame ();
+            }
+        }
+    }
+    public void ResumeGame () {
+        pauseMenu.SetActive (false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+    public void PauseGame () {
+        pauseMenu.SetActive (true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+    public void quitgame () {
+        Application.Quit ();
+        Debug.Log ("Quit");
+    }
+
+    public void LoadMainMenu (string LoadMainMenu) {
+        SceneManager.LoadScene ("MainMenu");
+        Time.timeScale = 1f;
+    }
+    public void RestartScene (string SampleScene) {
+        SceneManager.LoadScene ("SampleScene");
+        Time.timeScale = 1f;
+    }
+}
